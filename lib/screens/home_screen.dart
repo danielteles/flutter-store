4a97729134d379e5f1c20464afeb02f0da065dae
Daniel Teles <danieelteles@gmail.com>
2020-04-25 09:17:45 -0300

    Implementing some navigation and creating products tab

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:store/tabs/home_tab.dart';
+import 'package:store/tabs/products_tab.dart';
 import 'package:store/w9dgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,7 +16,13 @@ class HomeScreen extends StatelessWidget {
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
         ),
-        Container(color: Colors.red),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Products"),
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
         Container(color: Colors.green),
         Container(color: Colors.blue),
       ],
