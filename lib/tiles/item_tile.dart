710f333cfde90a7c6608054e4d20bdbb6bdea5bb
Daniel Teles <danieelteles@gmail.com>
2020-04-25 10:55:05 -0300

    Implementing grid

--- lib/tiles/item_tile.dart
+++ lib/tiles/item_tile.dart
@@ -3,12 +3,45 @@ import 'package:store/data/product_data.dart';
 
 class ItemTile extends StatelessWidget {
   final String type;
-  final ProductData data;
+  final ProductData product;
 
-  ItemTile(this.type, this.data);
+  ItemTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      child: Card(
+        child: type == "grid"
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Image.network(product.images[0], fit: BoxFit.cover),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text(product.title,
+                              style: TextStyle(fontWeight: FontWeight.w500)),
+                          Text(
+                            "R\$ ${product.price.toStringAsFixed(2)}",
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : Row(),
+      ),
+    );
   }
 }
